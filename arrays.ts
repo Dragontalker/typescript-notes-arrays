7efc24874be7f9d4f8e484388f9392b80ccd5942
Richard <richard.yang.tong@gmail.com>
2021-04-16 15:07:57 -0400

    Added type annotation for nested array.

--- arrays.ts
+++ arrays.ts
@@ -3,3 +3,9 @@ const carMakers: string[] = [];
 carMakers.push('ford');
 carMakers.push('toyota');
 carMakers.push('chevy');
+
+const cardsByMake: string[][] = [
+  ['f150'],
+  ['corolla'],
+  ['camaro']
+];
