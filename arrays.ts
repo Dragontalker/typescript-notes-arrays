734c507342199ac0c9ba560db62894863585182f
Richard <richard.yang.tong@gmail.com>
2021-04-16 15:11:27 -0400

    Added a case where ts prevents error.

--- arrays.ts
+++ arrays.ts
@@ -13,3 +13,6 @@ const cardsByMake: string[][] = [
 // Help with inference when extracting values
 const car = carMakers[0];
 const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
