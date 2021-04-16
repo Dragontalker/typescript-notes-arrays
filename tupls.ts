0a54b31069f4deaa50989e37c641f722094c3b7e
Richard <richard.yang.tong@gmail.com>
2021-04-16 15:33:43 -0400

    Added more examples.

--- tupls.ts
+++ tupls.ts
@@ -4,8 +4,9 @@ const drink = {
   sugar: 40
 };
 
+// Type alias
 type Drink = [string, boolean, number];
 
 const pepsi: Drink = ['brown', true, 40];
-pepsi[0] = 40;
-pepsi[2] = 'brown';
+const sprite: Drink = ['clear', true, 30];
+const tea: Drink = ['brown', false, 0];
